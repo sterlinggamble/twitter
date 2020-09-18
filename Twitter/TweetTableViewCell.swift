280ffda769ec38636cdb7d270a7214e1060b029a
Sterling <sterlingsgamble@gmail.com>
2020-09-17 21:04:45 -0700

    first commit

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Sterling Gamble on 9/17/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
